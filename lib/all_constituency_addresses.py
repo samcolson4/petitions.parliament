7994882fabb51c8d248811d028cf780eee02d03b
samcolson4 <samcolson4@gmail.com>
2021-04-20 16:26:46 +0100

    Add constituency link. Lint

--- lib/all_constituency_addresses.py
+++ lib/all_constituency_addresses.py
@@ -8,30 +8,34 @@ from constituency import Constituency
 
 
 def importConstituencies():
-    constituencies = []
+  constituencies = []
 
-    with open('constituencies.csv', mode='r') as f:
-      reader = csv.reader(f)
+  with open('constituencies.csv', mode='r') as f:
+    reader = csv.reader(f)
 
-      for row in reader:
-        if row[0] == 'Constituency':
-          continue
-        else: 
-          # print(row[0])
-          constituencies.append(Constituency(row[0], row[1], row[2]))
-    
-    return constituencies
+    for row in reader:
+      if row[0] == 'Constituency':
+        continue
+      else: 
+        # print(row[0])
+        constituencies.append(Constituency(row[0], row[1], row[2]))
+  
+  return constituencies
 
 
 def getURLS(constituencies):
-    base_url = 'https://petition.parliament.uk/petitions/local/'
+  base_url = 'https://petition.parliament.uk/petitions/local/'
 
-    for constituency in constituencies:
-      print("\nChecking constituency name")
-      add_url = constituency.constituency.replace(" ", "-")
-      full_url = base_url + add_url.lower()
-      print(full_url)
+  for constituency in constituencies:
+    print("\nChecking constituency name")
+    add_url = constituency.constituency.replace(" ", "-")
+    full_url = base_url + add_url.lower()
+    constituency.link = full_url
+    print(constituency.constituency)
+    print(constituency.link)
+  
+  return constituencies
 
 list = importConstituencies()
-# print(len(list))
+
 getURLS(list)
